59d80aa03b754b70f3c0d80e108aa24667789654
Andres Fuentes Hernandez <andres7233@hotmail.com>
2023-06-20 23:05:12 -0600

    rest-api: Changes to compile project to ar format.

--- rest-api/src/main/java/com/afh/choice/config/AppConfig.java
+++ rest-api/src/main/java/com/afh/choice/config/AppConfig.java
@@ -3,9 +3,10 @@ package com.afh.choice.config;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages = "com.afh.choice")
-public class AppConfig {
+public class AppConfig implements WebMvcConfigurer {
 }
