229a880b2443f6287d2cba599cb76c36bf5aa236
Andres Fuentes Hernandez <andres7233@hotmail.com>
2023-06-20 22:43:54 -0600

    rest-api: Hello World code for tomcat and spring app, running on docker.

---
+++ rest-api/src/main/java/com/afh/choice/config/AppConfig.java
@@ -0,0 +1,11 @@
+package com.afh.choice.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "com.afh.choice")
+public class AppConfig {
+}
