9512f37689bf63debf5d1f91975e5bc9514a2fd3
Andres Fuentes Hernandez <andres7233@hotmail.com>
2023-06-21 22:12:02 -0600

    soap-api: Initial soap api service.

---
+++ soap-api/src/main/java/com/afh/choice/configuration/HotelManagementInternalApplication.java
@@ -0,0 +1,13 @@
+package com.afh.choice.configuration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+
+@SpringBootApplication(scanBasePackages = "com.afh.choice", exclude = SecurityAutoConfiguration.class)
+public class HotelManagementInternalApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HotelManagementInternalApplication.class, args);
+    }
+
+}
