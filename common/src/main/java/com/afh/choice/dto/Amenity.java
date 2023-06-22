c25310c50023941a5571bc5bb466518b46b6293f
Andres Fuentes Hernandez <andres7233@hotmail.com>
2023-06-22 14:35:17 -0600

    common: Create common library for services.

---
+++ common/src/main/java/com/afh/choice/dto/Amenity.java
@@ -0,0 +1,7 @@
+package com.afh.choice.dto;
+
+public class Amenity {
+    private long id;
+    private String name;
+    private String description;
+}
---
+++ common/src/main/java/com/afh/choice/dto/Hotel.java
@@ -0,0 +1,13 @@
+package com.afh.choice.dto;
+
+import java.util.Set;
+
+public class Hotel {
+    private long id;
+    private String name;
+    private String address;
+
+    private int rating;
+
+    private Set<Amenity> Amenities;
+}
