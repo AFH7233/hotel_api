470c1da8731439b39702347b394d28e8a2e15c06
Andres Fuentes Hernandez <andres7233@hotmail.com>
2023-06-22 22:24:02 -0600

    common: Add getter and setters.

--- common/src/main/java/com/afh/choice/dto/Amenity.java
+++ common/src/main/java/com/afh/choice/dto/Amenity.java
@@ -4,4 +4,28 @@ public class Amenity {
     private long id;
     private String name;
     private String description;
+
+    public long getId(){
+        return this.id;
+    }
+
+    public void setId(long amenityId) {
+        this.id = amenityId;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String amenityName) {
+        this.name = amenityName;
+    }
+
+    public String getDescription(){
+        return this.description;
+    }
+
+    public void setDescription(String amenityDescription) {
+        this.description = amenityDescription;
+    }
 }
--- common/src/main/java/com/afh/choice/dto/Hotel.java
+++ common/src/main/java/com/afh/choice/dto/Hotel.java
@@ -1,6 +1,8 @@
 package com.afh.choice.dto;
 
+import java.util.Optional;
 import java.util.Set;
+import java.util.TreeSet;
 
 public class Hotel {
     private long id;
@@ -9,5 +11,44 @@ public class Hotel {
 
     private int rating;
 
-    private Set<Amenity> Amenities;
+    private Set<Amenity> amenities;
+
+    public void setId(long hotelId) {
+        this.id = hotelId;
+    }
+
+    public void setName(String hotelName) {
+        this.name = hotelName;
+    }
+
+    public void setAddress(String hotelAddress) {
+        this.address = address;
+    }
+
+    public void setRating(int hotelRating) {
+        this.rating = hotelRating;
+    }
+
+
+    public void setAmenities(Set<Amenity> amenities) {
+        this.amenities = new TreeSet<>(amenities);
+    }
+
+    public long getId() {
+        return this.id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getAddress() {
+        return this.address;
+    }
+
+
+    public int getRating() {
+        return this.rating;
+    }
+
 }
