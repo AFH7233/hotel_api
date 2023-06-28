c0188ee53b03cbda8c10d38bad0f99a60adcffc0
Andres Fuentes Hernandez <andres7233@hotmail.com>
2023-06-28 11:17:50 -0600

    rest-api: Add pretty formated responses when there is a duplication of hotels or the hotel/amenity does not exist.

---
+++ rest-api/src/main/java/com/afh/choice/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,31 @@
+package com.afh.choice.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.ws.soap.client.SoapFaultClientException;
+
+/**
+ * Holds common exceptions through the service.
+ *
+ * @author Andres Fuentes Hernandez
+ */
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(SoapFaultClientException.class)
+    public ResponseEntity<?> handleSOAPException(
+            SoapFaultClientException ex, WebRequest request) {
+        String fault = ex.getFaultStringOrReason();
+        if (fault.contains("Duplicate entry")) {
+            return new ResponseEntity<>("The hotel name must be unique, all names are trimmed and converted to lower case.", HttpStatus.NOT_ACCEPTABLE);
+        } else if (fault.contains("Incorrect result size")) {
+            return new ResponseEntity<>("The hotel/amenity does not exist.", HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(ex.getFaultStringOrReason(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+
+}
